06171d09cb98eabfaa8005b1b212449fedab3cea
rachit05 <rachit.shrivastava70@gmail.com>
2021-10-07 14:11:54 +0530

    fixed favicon issue

--- sw.js
+++ sw.js
@@ -1,14 +1,21 @@
 self.addEventListener('install',function(event){
+    console.log('[Service Worker] Installing...')
     event.waitUntil(
         caches.open('sw-cache').then(function(cache){
-            return cache.addAll(['index.html','style.css','main.js','list.json']);
+            return cache.addAll(['index.html','style.css','main.js','list.json','manifest.json','Favicon']);
         })
     );
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating...');
 });
 
 self.addEventListener('fetch',function(event){
     event.respondWith(
         caches.match(event.request).then(function(response){
+            console.log('[Service Worker Installed Successfully]')
+
             return response || fetch(event.request);
         })
     )
