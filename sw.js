6f476011b6b78438bce0fd2c7464c9faf2b73a68
rachit05 <rachit.shrivastava70@gmail.com>
2021-10-07 14:29:24 +0530

    changed the path inside of the sw.js file

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-self.addEventListener('install',function(event){
+self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing...')
     event.waitUntil(
-        caches.open('sw-cache').then(function(cache){
-            return cache.addAll(['./index.html','./style.css','./main.js','./list.json','./manifest.json','./Favicon']);
+        caches.open('sw-cache').then(function (cache) {
+            return cache.addAll(['/index.html', '/style.css', '/main.js', '/list.json', '/manifest.json', '/Favicon/']);
         })
     );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating...');
 });
 
-self.addEventListener('fetch',function(event){
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response){
+        caches.match(event.request).then(function (response) {
             console.log('[Service Worker Installed Successfully]')
 
             return response || fetch(event.request);
